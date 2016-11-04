f5523f1f0934b48d924bfce893e916ab63daf8de
Pedro Souza <pedro@vencis.com.br>
2016-11-04 11:56:05 -0200

    Simple todo list using react

--- src/components/ListItem.jsx
+++ src/components/ListItem.jsx
@@ -2,14 +2,14 @@ var React = require('react');
 
 var ListItem = React.createClass({
 
-    render : function () {
-        return (
+    render: function () {
+        return(
             <li>
-                <h4>{this.props.ingredient}</h4>
+                <h4>{this.props.text}</h4>
             </li>
         );
     }
-    
+
 });
 
 module.exports = ListItem;
